1fdaf9efe2a2ba800a969657678f27ba75aab915
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-05 14:40:48 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class person
+  def name+(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(person_job)
+    @job = persone_job
+  end
+  
+  def job
+    @job
+  end
+end
